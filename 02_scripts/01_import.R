9b9caeaa2d687b1f98b53cb9d35acce59306925a
mylinhluong <mylinhl@student.unimelb.edu.au>
2020-01-22 14:19:53 -0700

    this script has been simplified to load libraries and import self-report and IAT datasets

--- 02_scripts/01_import.R
+++ 02_scripts/01_import.R
@@ -1,10 +1,7 @@
-#this script includes all libraries needed for scripts, imports data, ?should functions be included here or in a separate R script?
-
+#this script includes all libraries needed for scripts, and imports the self-report and IAT data 
 #todo
 #should libraries be loaded here or by script?
-#should functions be loaded here or by script?
-#check on packrat MICE package to see if it working appropriately
-#make sure to decide whether or not the package DATA.TABLE will be used or not.
+##make sure to decide whether or not the package DATA.TABLE will be used or not.
 
 #load libraries here
 library(here)
@@ -15,23 +12,6 @@ library(data.table)
 library(psych)
 library(mice)
 library(tidyr)
-
-#function for converting variable types#
-convert.magic <- function(obj, type){
-  FUN1 <- switch(type,
-                 character = as.character,
-                 numeric = as.numeric,
-                 factor = as.factor)
-  out <- lapply(obj, function(x) FUN1(as.character(x)))
-  as.data.frame(out)
-}
-
-
-
-"console message
-package ‘mice’ successfully unpacked and MD5 sums checked
-Error in install.packages : ERROR: failed to lock directory ‘C:\Users\My-Linh\Dropbox\University of Melbourne\Dissertation\CH 3 Non conscious processes\fictional-happiness\packrat\lib\x86_64-w64-mingw32\3.6.1’ for modifying
-Try removing ‘C:\Users\My-Linh\Dropbox\University of Melbourne\Dissertation\CH 3 Non conscious processes\fictional-happiness\packrat\lib\x86_64-w64-mingw32\3.6.1/00LOCK’
 
 
 #import IAT and self-report raw datasets into environment
